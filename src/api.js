193c154bd2790359860e487e6df2376f43a8f468
Rapenzol <sampadajadhav26123@gmail.com>
2025-09-11 12:14:08 +0530

    api is added inside base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-backend-production-e3a6.up.railway.app", // backend URL
+  baseURL: "https://task-manager-backend-production-e3a6.up.railway.app/api", // backend URL
 });
 
 export default api;
